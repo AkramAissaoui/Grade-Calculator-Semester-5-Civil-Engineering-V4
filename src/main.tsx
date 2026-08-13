637d0724ed63f3cc426396330cbad8afe5b118a9
AkramAissaoui <medakram422344@gmail.com>
2026-08-13 17:48:51 +0100

    Pass lang and onLanguageChange to HomePage

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
@@ -10,16 +10,24 @@ if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/year/y1" element={<App year="y1" lang="fr" />} />
-        <Route path="/year/y2" element={<App year="y2" lang="fr" />} />
-        <Route path="/year/y3" element={<App year="y3" lang="fr" />} />
-        <Route path="/year/y4" element={<App year="y4" lang="fr" />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+type Language = 'fr' | 'en' | 'ar';
+
+function Root() {
+  const [lang, setLang] = useState<Language>('fr');
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage lang={lang} onLanguageChange={setLang} />} />
+          <Route path="/year/y1" element={<App year="y1" lang={lang} />} />
+          <Route path="/year/y2" element={<App year="y2" lang={lang} />} />
+          <Route path="/year/y3" element={<App year="y3" lang={lang} />} />
+          <Route path="/year/y4" element={<App year="y4" lang={lang} />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+root.render(<Root />);
